4794090dabede89b49f39a286364b7f151ee14a5
Oluwanifemi <theoluwanifemi@gmail.com>
2026-01-15 23:24:29 +0100

    Fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://momentos-production.up.railway.app",
+        target: "https://momentos-production.up.railway.app/api",
         changeOrigin: true,
       },
     },
